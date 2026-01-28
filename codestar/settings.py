3020d52100d8fda5cf1455aee5e29a60c0872dce
Verity Allison <verityalls@gmail.com>
2026-01-28 12:19:19 +0000

    add debug code

--- codestar/settings.py
+++ codestar/settings.py
@@ -28,7 +28,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+# DEBUG = False
+DEBUG = "DEVELOPMENT" in os.environ
+
 
 ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1',]
 
