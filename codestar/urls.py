15f505e524a8fed830a9ce9330404ef7a5a56c8e
Verity Allison <verityalls@gmail.com>
2026-01-29 12:42:36 +0000

    add url.py to blog

--- codestar/urls.py
+++ codestar/urls.py
@@ -15,11 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from blog.views import my_blog
+from django.urls import path, include
+# from blog.views import my_blog - no longer needed as blog urls are included
 
 
 urlpatterns = [
+    path("", include("blog.urls"), name="blog-urls"),
     path('admin/', admin.site.urls),
-    path('blog/', my_blog, name='blog'),
 ]
