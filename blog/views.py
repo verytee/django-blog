837494ab9f06d2e5cc6dc87ce32e58a637649f2b
Verity Allison <verityalls@gmail.com>
2026-01-29 13:08:06 +0000

    generic post view

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Post
 
 # Create your views here.
+
+
+class PostList(generic.ListView):
+    queryset = Post.objects.all()
+    template_name = "post_list.html"
