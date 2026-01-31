1c32e251f272768df4ca12967c42e00ff3a95d63
Verity Allison <verityalls@gmail.com>
2026-01-31 18:20:21 +0000

    add about url

--- codestar/urls.py
+++ codestar/urls.py
@@ -16,12 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from about import views as about_views
 # from blog.views import my_blog - no longer needed as blog urls are included
 
 
 urlpatterns = [
-    path('about/', about_views.about_me, name='about'),
+    path('about/', include("about.urls"), name="about-urls"),
     path('admin/', admin.site.urls),
     path('summernote/', include('django_summernote.urls')),
     path("", include("blog.urls"), name="blog-urls"),
