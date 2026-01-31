e10f500034a76e5b170060994832a2550c6c8ab0
Verity Allison <verityalls@gmail.com>
2026-01-31 18:16:45 +0000

    add about.html

--- about/views.py
+++ about/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import About
 
 # Create your views here.
 
+
 def about_me(request):
-    return HttpResponse("This is the About Me page.")
+    """
+    Renders the About page
+    """
+    about = About.objects.all().order_by('-updated_on').first()
+
+    return render(
+        request,
+        "about/about.html",
+        {"about": about},
+    )
